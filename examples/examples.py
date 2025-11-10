a643943f6ae7414afcfafad8c1b3cbabe7c5d804
Matthew Dorsey <madorse2@ncsu.edu>
2025-11-10 09:51:38 -0600

    Created example of scatter plot for testing.

---
+++ examples/examples.py
@@ -0,0 +1,42 @@
+
+## Matthew Dorsey
+## @sunprancekid
+## 2025.11.10
+
+## FILENAME: examples/examples.py
+## PURPOUSE: demonstrate how plotfig package works
+
+## MODULES
+# native / conda
+import sys, os
+# local
+from plot.figure import Figure
+from plot.plot import gen_plot
+
+## CONSTANTS / PARAMETERS
+# none
+
+## METHODS 
+# none
+
+## CLASSES
+# none
+
+## ARGUMENTS
+# boolean for running the scatter example
+scatter = ('scatter' in sys.argv) or ('all' in sys.argv)
+
+## SCRIPT
+if scatter: # run scatter example
+	# load data from csv
+	fig = Figure()
+	fig.append_from_csv('data/scatter.csv', xcol = 'mag_val', ycol = 'ss_val', ccol = 'den', label = 'SS')
+	fig.append_from_csv('data/scatter.csv', xcol = 'mag_val', ycol = 'ht_val', ccol = 'den', label = 'HT')
+	# adjust plot attributes
+	fig.set_xaxis_label('Magnetism Order Parameter ($M$)')
+	fig.set_yaxis_label('$SS$ or $HT$ Order Parameter')
+	fig.set_label(ival = "HT", label = "Head-to-Tail ($HT$)")
+	fig.set_label(ival = "SS", label = "Side-to-Side ($SS$)")
+	fig.reset_markers(['D', '^'])
+	# show scatter
+	gen_plot(fig, linewidth = 0, legendloc = 'lower center', save = False)
