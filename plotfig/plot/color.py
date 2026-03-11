90829f56132a6edf5669fa91e7c6c81782af3b40
Matthew Dorsey <madorse2@ncsu.edu>
2026-03-11 14:15:09 +0100

    Added initial color plot.

---
+++ plotfig/plot/color.py
@@ -0,0 +1,85 @@
+
+## Matthew A. Dorsey
+## @sunprancekid
+## 2026.02.24
+
+## FILENAME: plot/color.py
+## PURPOSE: contains methods for handling generation of colors and schemes
+
+## MODULES
+# native / conda
+import sys, os
+# local
+# none
+
+
+## PARAMETERS
+# none
+
+
+## METHODS
+# none
+
+
+## CLASSES
+# color class
+class Color (object):
+	""" used to describe / format individual colors.
+
+	Attributes:
+	-----------
+	None
+
+	Methods:
+	--------
+	None
+	"""
+
+	def __init__ (self):
+		""" initialized color object.
+
+		Arguments:
+		----------
+		None
+
+		Returns:
+		--------
+		Color
+			initialized color object.
+		"""
+		pass
+
+
+# scheme class
+class Scheme (object):
+	""" handles color sets.
+
+	Attributes:
+	-----------
+	None
+
+	Methods:
+	--------
+	None
+	"""
+
+	def __init__ (self):
+		""" initialize color scheme object.
+
+		Arguments:
+		----------
+		None
+
+		Returns:
+		--------
+		None
+		"""
+		pass
+
+
+## ARGUMENTS
+# none
+
+
+# SCRIPT
+# none
